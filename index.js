a3495f7ccc27398d817800e497cca5a47d6536e5
dee022 <133804910+dee022@users.noreply.github.com>
2023-08-11 17:53:58 +0530

    secon

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const adminRouter = require("./routes/admin");
@@ -12,10 +12,18 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-
 // Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb+srv://Vipul:Vipul123@cluster0.vth9opv.mongodb.net//', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Courses" });
+ mongoose.connect('mongodb+srv://Vipul:Vipul123@cluster0.vth9opv.mongodb.net//', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Courses" });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+// step 3: Heroku 
+if ( process.env.NODE_ENV == "production"){
+    app.use(express.static("client/dist"));
+    const path = require("path");
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // })
+}  
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
